15c2108d98bf5cc98f20bc7fa48d8e74598b66ab
Heejin Cho <74577803+choheejin@users.noreply.github.com>
2022-12-23 23:28:36 +0900

    Update 10773.py

--- 구현/10773.py
+++ 구현/10773.py
@@ -18,3 +18,19 @@ for num in num_zip:
 
 print(result)
     
+
+# 다른 사람 코드 보고 코드 최적화 -> 불필요한 for 문 제거
+import sys
+
+n = int(sys.stdin.readline())
+
+num_zip = []
+
+for i in range(n):
+  num = int(sys.stdin.readline())
+  if(num == 0 and len(num_zip) > 0):
+    del num_zip[-1]
+  elif(num != 0):
+    num_zip.append(num)
+
+print(sum(num_zip))
